e08566c9f07c2b146b48942c2ea955a6644856b1
couscoudet <ysoultane@gmail.com>
2025-12-20 08:59:52 +0100

    fix session end delay to 7 days intead of 1 second

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -66,7 +66,7 @@ export class AuthController {
       httpOnly: true,
       secure: isProduction,
       sameSite: isProduction ? 'none' : 'lax',
-      maxAge: 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
   }
